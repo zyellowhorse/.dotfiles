f026de38c01125de3ae8fee0a6ae489d2faabb6a
zyellowhorse <zacharyyellowhorse@gmail.com>
2022-01-30 11:15:38 -0700

    Updated Telescope filebrowser dir icon to not be invalid character and updated filter to only filter out the git directory

--- nvim/.config/nvim/vim-plug/telescope.lua
+++ nvim/.config/nvim/vim-plug/telescope.lua
@@ -1,13 +1,18 @@
 local telescope = require('telescope')
 telescope.setup {
     defaults = {
-        file_ignore_patterns = {".git"}
+        file_ignore_patterns = {".git/"}
     },
     pickers = {
         find_files = {
             hidden = true
         }
-    }    
+    },
+    extensions = {
+        file_browser = {
+            dir_icon = "├"
+        }
+    }
 }
 
 require("telescope").load_extension "file_browser"
